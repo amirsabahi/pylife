3cc8387ae0862bdb9516d02acaf3973b584073fb
amirsabahi <amir.sabahi@gmail.com>
2025-05-26 07:17:37 -0400

    3sum code change

--- practice/3sum.py
+++ practice/3sum.py
@@ -48,12 +48,12 @@ class Solution(object):
         return result          
 
 sln = Solution()
-# sln.threeSum([-1,0,1,2,-1,-4])    
-# sln.threeSum([0,1,-1])     
-# sln.threeSum([0,0,0,])  
-# sln.threeSum([0,0,0,0,])  
-# sln.threeSum([1,2,-2,-1])  
-# sln.threeSum([-2,0,1,1,2])  
+sln.threeSum([-1,0,1,2,-1,-4])    
+sln.threeSum([0,1,-1])     
+sln.threeSum([0,0,0,])  
+sln.threeSum([0,0,0,0,])  
+sln.threeSum([1,2,-2,-1])  
+sln.threeSum([-2,0,1,1,2])  
 sln.threeSum([2,-3,0,-2,-5,-5,-4,1,2,-2,2,0,2,-4,5,5,-10])  
 
 
